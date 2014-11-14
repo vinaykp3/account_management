e3145adb03ade76b4306a63fa4a0f81ae0bc7e75
Vinay Kolathuru Puttaswamy <vinaykp3@gmail.com>
2014-11-14 21:16:27 +0530

    uncommenting request referrer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 	def index
-		# if request.referrer.nil?
-	 #      redirect_to $redis.get("user_#{current_user.id}")
-	 #    else  
-	 #      redirect_to root_path
-	 #    end
+          if request.referrer.nil?
+	    redirect_to $redis.get("user_#{current_user.id}")
+	  else  
+	    redirect_to root_path
+	  end
 	end
 end	
